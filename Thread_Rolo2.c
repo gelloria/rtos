c24b171dbd92c6bad6f2c267d60b82ee93beb4d2
Rolando Moraga Mora <rolandommora123@gmail.com>
2019-10-09 19:55:42 -0600

    adding first code with timer

--- Thread_Rolo2.c
+++ Thread_Rolo2.c
@@ -6,29 +6,49 @@
 #include <sys/time.h>
 #include "mythreads.c"
 
-#define SECOND 1000000
+#define TIME_TO_SLEEP 250000 //us
 #define Number_Of_Threads 3
 #define STACK_SIZE 40096
+#define QUANTUM 2000 //ms
 
 int thread_counter = 0;
 int next_thread;
+
+struct thread_metadata {
+  int bills;
+  int workload;
+};
 
 struct threads_type{
   int id;
   sigjmp_buf env;
   char stack[STACK_SIZE];
   void *(function);
-  int arg1;
-  float result;
-  int lottery_weight;
-
+  struct thread_metadata thread_data;
 };
+
+void timer_quantum(int quantum_ms, void *function){
+	struct itimerval timer;
+
+  int sec = quantum_ms / 1000;
+  int usec = (quantum_ms % 1000 ) * 1000;
+
+   /* Configure the timer to expire after X sec... */
+   timer.it_value.tv_sec = sec;
+   timer.it_value.tv_usec = usec;
+   /* ... and every X sec after that. */
+   timer.it_interval.tv_sec = sec;
+   timer.it_interval.tv_usec = usec;
+
+	signal(SIGALRM, function);
+	setitimer(ITIMER_REAL, &timer, NULL);
+}
 
 struct threads_type list_of_threads[Number_Of_Threads];
 struct threads_type current_thread;
 struct threads_type previous_thread;
 
-void dummy_scheduler(){
+void Scheduler(){
     int coming_back = sigsetjmp(list_of_threads[current_thread.id].env,1);
 
     if (coming_back == 1) {return;} //Returns no normal execution of the function.
@@ -40,32 +60,32 @@ void dummy_scheduler(){
     siglongjmp(current_thread.env,1);
 }
 
+void signal_handler(){
+	printf("\nTiempo expiró, saltando hilo\n\n");
+  Scheduler();
+}
 
 
 void sumador(void){
+    int i=0;
 
-    int arg = current_thread.arg1;
-    int i=arg;
+    timer_quantum(QUANTUM, signal_handler);
+
     while(1) {
         ++i;
-        printf("Sumando (%d) en hilo (%d), arg (%d)\n",i, current_thread.id, arg);
-        if (i % 3 == 0) {
-            printf("sumador: switching\n");
-            signal(SIGALRM, SIG_DFL);
-            dummy_scheduler();
-        }
-        usleep(SECOND);
+        printf("Sumando (%d) en hilo (%d)\n",i, current_thread.id);
+        usleep(TIME_TO_SLEEP);
     }
 }
 
 
-void create_hilo(void *function, int arg) {
+void create_hilo(void *function, struct thread_metadata thread_data) {
 
   printf("Creando Hilo %d\n", thread_counter);
 
   list_of_threads[thread_counter].id = thread_counter;
   list_of_threads[thread_counter].function = function;
-  list_of_threads[thread_counter].arg1 = arg;
+  list_of_threads[thread_counter].thread_data = thread_data;
 
   address_t sp, pc;
 
@@ -82,14 +102,20 @@ void create_hilo(void *function, int arg) {
 
 int main(){
 
+  struct thread_metadata thread_1;
+  struct thread_metadata thread_2;
+  struct thread_metadata thread_3;
 
-    create_hilo(sumador, 69);
-    create_hilo(sumador, 80);
-    create_hilo(sumador, 30);
+  thread_1.workload = 50;
+  thread_1.workload = 250;
+  thread_1.workload = 150;
 
-    int Thread_To_Run = 0;
+    create_hilo(sumador, thread_1);
+    create_hilo(sumador, thread_2);
+    create_hilo(sumador, thread_3);
 
-    current_thread = list_of_threads[Thread_To_Run];
+
+    current_thread = list_of_threads[0];
 
     siglongjmp(current_thread.env,1);
     return 0;
