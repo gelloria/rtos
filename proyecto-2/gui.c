82005d8ce55a4eec611f4b3f6ad2f28291faf1f4
Leonardo Araya <leoam@hotmail.es>
2019-11-27 06:57:33 -0600

    Finish GUI

--- proyecto-2/gui.c
+++ proyecto-2/gui.c
@@ -22,6 +22,10 @@ GtkWidget *label_task_5;
 GtkWidget *label_period;
 GtkWidget *label_exec_time;
 GtkWidget *label_note;
+GtkWidget *checkbutton_rm;
+GtkWidget *checkbutton_edf;
+GtkWidget *checkbutton_llf;
+GtkWidget *checkbutton_multiple_page;
 
 const gchar *entry_0_text_exec_time;
 const gchar *entry_1_text_exec_time;
@@ -36,10 +40,33 @@ const gchar *entry_3_text_period;
 const gchar *entry_4_text_period;
 const gchar *entry_5_text_period;
 
-static void run_scheduler (void)
-{
-  int entry_0_exec_time_int;
+int use_rm = 0;
+int use_edf = 0;
+int use_llf = 0;
+int use_multiple_page = 0;
 
+int entry_0_exec_time_int;
+int entry_1_exec_time_int;
+int entry_2_exec_time_int;
+int entry_3_exec_time_int;
+int entry_4_exec_time_int;
+int entry_5_exec_time_int;
+
+int entry_0_text_period_int;
+int entry_1_text_period_int;
+int entry_2_text_period_int;
+int entry_3_text_period_int;
+int entry_4_text_period_int;
+int entry_5_text_period_int;
+
+void get_checkbox_data(){
+  use_rm = gtk_toggle_button_get_active (GTK_TOGGLE_BUTTON (checkbutton_rm));
+  use_edf = gtk_toggle_button_get_active (GTK_TOGGLE_BUTTON (checkbutton_edf));
+  use_llf = gtk_toggle_button_get_active (GTK_TOGGLE_BUTTON (checkbutton_llf));
+  use_multiple_page = gtk_toggle_button_get_active (GTK_TOGGLE_BUTTON (checkbutton_multiple_page));
+}
+
+void get_entries_data(){
   // Get the execution time
   entry_0_text_exec_time = gtk_entry_get_text (GTK_ENTRY (entry_0_exc_time));
   entry_1_text_exec_time = gtk_entry_get_text (GTK_ENTRY (entry_1_exc_time));
@@ -47,9 +74,6 @@ static void run_scheduler (void)
   entry_3_text_exec_time = gtk_entry_get_text (GTK_ENTRY (entry_3_exc_time));
   entry_4_text_exec_time = gtk_entry_get_text (GTK_ENTRY (entry_4_exc_time));
   entry_5_text_exec_time = gtk_entry_get_text (GTK_ENTRY (entry_5_exc_time));
-
-  entry_0_exec_time_int = atoi(entry_0_text_exec_time);
-  g_print("NUUUUUM %d", entry_0_exec_time_int);
 
   // Get the period
   entry_0_text_period = gtk_entry_get_text (GTK_ENTRY (entry_0_period));
@@ -59,25 +83,72 @@ static void run_scheduler (void)
   entry_4_text_period = gtk_entry_get_text (GTK_ENTRY (entry_4_period));
   entry_5_text_period = gtk_entry_get_text (GTK_ENTRY (entry_5_period));
 
-  g_print ("entry_0_exc_time contents: %s\n", entry_0_text_exec_time);
-  g_print ("entry_1_exc_time contents: %s\n", entry_1_text_exec_time);
-  g_print ("entry_2_exc_time contents: %s\n", entry_2_text_exec_time);
-  g_print ("entry_3_exc_time contents: %s\n", entry_3_text_exec_time);
-  g_print ("entry_4_exc_time contents: %s\n", entry_4_text_exec_time);
-  g_print ("entry_5_exc_time contents: %s\n", entry_5_text_exec_time);
+  // Cast from string to int
+  entry_0_exec_time_int = atoi(entry_0_text_exec_time);
+  entry_1_exec_time_int = atoi(entry_1_text_exec_time);
+  entry_2_exec_time_int = atoi(entry_2_text_exec_time);
+  entry_3_exec_time_int = atoi(entry_3_text_exec_time);
+  entry_4_exec_time_int = atoi(entry_4_text_exec_time);
+  entry_5_exec_time_int = atoi(entry_5_text_exec_time);
 
-  g_print ("entry_0_period contents: %s\n", entry_0_text_period);
-  g_print ("entry_1_period contents: %s\n", entry_1_text_period);
-  g_print ("entry_2_period contents: %s\n", entry_2_text_period);
-  g_print ("entry_3_period contents: %s\n", entry_3_text_period);
-  g_print ("entry_4_period contents: %s\n", entry_4_text_period);
-  g_print ("entry_5_period contents: %s\n", entry_5_text_period);
+  entry_0_text_period_int = atoi(entry_0_text_period);
+  entry_1_text_period_int = atoi(entry_1_text_period);
+  entry_2_text_period_int = atoi(entry_2_text_period);
+  entry_3_text_period_int = atoi(entry_3_text_period);
+  entry_4_text_period_int = atoi(entry_4_text_period);
+  entry_5_text_period_int = atoi(entry_5_text_period);
 
+  // Print the results
+  g_print ("entry_0_exc_time contents: %d\n", entry_0_exec_time_int);
+  g_print ("entry_1_exc_time contents: %d\n", entry_1_exec_time_int);
+  g_print ("entry_2_exc_time contents: %d\n", entry_2_exec_time_int);
+  g_print ("entry_3_exc_time contents: %d\n", entry_3_exec_time_int);
+  g_print ("entry_4_exc_time contents: %d\n", entry_4_exec_time_int);
+  g_print ("entry_5_exc_time contents: %d\n", entry_5_exec_time_int);
+
+  g_print ("entry_0_period contents: %d\n", entry_0_text_period_int);
+  g_print ("entry_1_period contents: %d\n", entry_1_text_period_int);
+  g_print ("entry_2_period contents: %d\n", entry_2_text_period_int);
+  g_print ("entry_3_period contents: %d\n", entry_3_text_period_int);
+  g_print ("entry_4_period contents: %d\n", entry_4_text_period_int);
+  g_print ("entry_5_period contents: %d\n", entry_5_text_period_int);
 }
 
-static void
-activate (GtkApplication *app,
-          gpointer        user_data)
+int verify_values(){
+  if(entry_0_exec_time_int > entry_0_text_period_int){
+    g_print("In the task 0 the execution time is greater than the period. Insert right values.\n");
+    return 1;
+  }else if(entry_1_exec_time_int > entry_1_text_period_int){
+    g_print("In the task 1 the execution time is greater than the period. Insert right values.\n");
+    return 1;
+  }else if(entry_2_exec_time_int > entry_2_text_period_int){
+    g_print("In the task 2 the execution time is greater than the period. Insert right values.\n");
+    return 1;
+  }else if(entry_3_exec_time_int > entry_3_text_period_int){
+    g_print("In the task 3 the execution time is greater than the period. Insert right values.\n");
+    return 1;
+  }else if(entry_4_exec_time_int > entry_4_text_period_int){
+    g_print("In the task 4 the execution time is greater than the period. Insert right values.\n");
+    return 1;
+  }else if(entry_5_exec_time_int > entry_5_text_period_int){
+    g_print("In the task 5 the execution time is greater than the period. Insert right values.\n");
+    return 1;
+  }
+
+  return 0;
+}
+
+static void run_scheduler (void)
+{
+  get_checkbox_data();
+  get_entries_data();
+  int values_are_wrong = verify_values();
+
+  // TODO. En lugar de hacer exit, no llamar al scheduler
+  if(values_are_wrong) exit(0);
+}
+
+static void activate(GtkApplication *app, gpointer user_data)
 {
   GtkWidget *window;
   GtkWidget *grid;
@@ -97,7 +168,7 @@ activate (GtkApplication *app,
   label_period = gtk_label_new("Period");
   gtk_grid_attach (GTK_GRID (grid), label_period, 2, 0, 1, 1);
   label_note = gtk_label_new("Note: leave the fields empty to not use these tasks");
-  gtk_grid_attach (GTK_GRID (grid), label_note, 0, 7, 3, 2);
+  gtk_grid_attach (GTK_GRID (grid), label_note, 0, 7, 3, 1);
 
   label_task_0 = gtk_label_new("Task 0");
   gtk_grid_attach (GTK_GRID (grid), label_task_0, 0, 1, 1, 1);
@@ -138,8 +209,18 @@ activate (GtkApplication *app,
   entry_5_period = gtk_entry_new ();
   gtk_grid_attach (GTK_GRID (grid), entry_5_period, 2, 6, 1, 1);
 
+  // Create the checkbox to choose the algorithm
+  checkbutton_rm = gtk_check_button_new_with_label ("Rate Monothonic");
+  gtk_grid_attach (GTK_GRID (grid), checkbutton_rm, 0, 8, 3, 1);
+  checkbutton_edf = gtk_check_button_new_with_label ("Earliest Deadline First");
+  gtk_grid_attach (GTK_GRID (grid), checkbutton_edf, 0, 9, 3, 1);
+  checkbutton_llf = gtk_check_button_new_with_label ("Least Laxity First");
+  gtk_grid_attach (GTK_GRID (grid), checkbutton_llf, 0, 10, 3, 1);
+  checkbutton_multiple_page = gtk_check_button_new_with_label ("Multiple page");
+  gtk_grid_attach (GTK_GRID (grid), checkbutton_multiple_page, 0, 11, 3, 1);
+
   button = gtk_button_new_with_label ("Schedule");
-  gtk_grid_attach (GTK_GRID (grid), button, 0, 9, 3, 1);
+  gtk_grid_attach (GTK_GRID (grid), button, 0, 12, 3, 1);
   g_signal_connect (button, "clicked", G_CALLBACK (run_scheduler), NULL);
 
   gtk_widget_show_all (window);
